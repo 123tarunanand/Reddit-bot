d91452196adffdaf81b87ec636d4032ba19e6469
123tarunanand <anandtarun2@gmail.com>
2017-12-17 23:30:18 +0530

    WOrking

--- dls/DLS.py
+++ dls/DLS.py
@@ -1,5 +1,6 @@
 import praw
 import config
+import pandas as pd
 #from pycricbuzz import Cricbuzz
 
 def bot_login():
@@ -12,13 +13,26 @@ def bot_login():
 
 
 def run_bot(r):
+    DLSTABLE = pd.read_csv('DLSTABLE.csv')
     #c = Cricbuzz()
-    subreddit = r.subreddit('cricket')
-    submission = subreddit.submissions()
-    for post in submission:
-        if "Match Thread" in post.title:
-            print(post.title)
-            print(post.selftext)
+    #subreddit = r.subreddit('cricket')
+    #submission = subreddit.submissions()
+    #for post in submission:
+        #if "Match Thread" in post.title:
+            #print(post.title)
+            #print(post.selftext
+    inital_over = 50
+    team1_score = 230
+    team2_initial_target = 300
+    team2_initial_overs = 40
+    for i in range(len(DLSTABLE)):
+        if DLSTABLE['O'][i] == team2_initial_overs:
+            break
+    team2_initialres = float(str(round(DLSTABLE['W0'][i],2)))
+    team1_resources = float(str(round(1/((team2_initial_target-1)/(team1_score*team2_initialres)),2)))
+    current_over
+
+
 
 
 
